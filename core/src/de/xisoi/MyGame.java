fc9996c78a3e999e9dcd7ba3f0310ff2f8d8dbc2
Xinoi <zaunrithj@gmail.com>
2022-03-27 14:44:31 +0200

    small changes

--- core/src/de/xisoi/MyGame.java
+++ core/src/de/xisoi/MyGame.java
@@ -1,6 +1,7 @@
 package de.xisoi;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,14 +13,14 @@ public class MyGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("starter-ship-red.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, Gdx.graphics.getWidth()/2 - img.getWidth(), Gdx.graphics.getHeight()/2 - img.getHeight(), 150, 150);
 		batch.end();
 	}
 	
