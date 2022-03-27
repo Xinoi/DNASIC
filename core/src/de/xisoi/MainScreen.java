96a3fb13132f441e6aded3bf1b24512ce6125182
Xinoi <zaunrithj@gmail.com>
2022-03-27 18:00:25 +0200

    added MainScreen

---
+++ core/src/de/xisoi/MainScreen.java
@@ -0,0 +1,34 @@
+package de.xisoi;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainScreen extends ScreenAdapter {
+
+    SpriteBatch batch;
+    Texture img;
+
+    public MainScreen() {
+       batch = new SpriteBatch();
+       img = new Texture("starter-ship-red.png");
+    }
+
+    @Override
+    public void render(float render) {
+        ScreenUtils.clear(0, 0, 0, 1);
+
+        batch.begin();
+        batch.draw(img, Gdx.graphics.getWidth()/2 - img.getWidth(), Gdx.graphics.getHeight()/2 - img.getHeight(), 150, 150);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
+
+}
