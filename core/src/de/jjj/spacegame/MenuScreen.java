da3cc7b6376f02848896c5a046f841bc3ebceaa5
Xinoi <zaunrithj@gmail.com>
2022-05-09 18:02:48 +0200

    added title and button implementation(not done, problem with TextureAtlas)

--- core/src/de/jjj/spacegame/MenuScreen.java
+++ core/src/de/jjj/spacegame/MenuScreen.java
@@ -7,22 +7,60 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MenuScreen extends ScreenAdapter {
 
     private SpriteBatch batch;
     private Music bgMusic;
-    private BitmapFont title;
+    private BitmapFont titleFont;
+
+    private Stage stage;
+
+    private TextureAtlas atlas;
+    private Skin skin;
+
+    private TextButton startB;
+    private TextButton settingsB;
+    private TextButton exitB;
+    private TextButton.TextButtonStyle bStyle;
+
+
 
     public MenuScreen() {
        batch = new SpriteBatch();
 
-       title = new BitmapFont(Gdx.files.internal("BitmapFonts/TitleFont.fnt"));
+       titleFont = new BitmapFont(Gdx.files.internal("BitmapFonts/TitleFont.fnt"));
 
+       //set a stage
+       stage = new Stage();
+       Gdx.input.setInputProcessor(stage);
+
+       //create a button
+       atlas = new TextureAtlas("TextureAtlas/packed/Button/buttons.atlas");
+       skin = new Skin(atlas);
+       bStyle = new TextButton.TextButtonStyle();
+       bStyle.font = titleFont;
+       bStyle.up = skin.getDrawable("button_up");
+       bStyle.down = skin.getDrawable("button_down");
+       bStyle.pressedOffsetX = 1;
+       bStyle.pressedOffsetY = -1;
+
+       startB = new TextButton("play", bStyle);
+
+       //background Music
        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Startmenu.wav"));
        bgMusic.setLooping(true);
        bgMusic.play();
+
+       //add things to stage
+       stage.addActor(startB);
+
     }
 
     @Override
@@ -31,14 +69,19 @@ public class MenuScreen extends ScreenAdapter {
 
         batch.begin();
         // dont know how to get the width of the text
-        title.draw(batch, "D N A S I C", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() - 200);
+        titleFont.draw(batch, "D N A S I C", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() - 200);
         batch.end();
+
+        stage.act(render);
+        stage.draw();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        title.dispose();
+        titleFont.dispose();
+        atlas.dispose();
+        skin.dispose();
     }
 
 }
