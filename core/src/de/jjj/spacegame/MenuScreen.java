83539d724950060c177da9442cbf8aeaab00f70c
Xinoi <zaunrithj@gmail.com>
2022-05-09 16:29:29 +0200

    added title Bitmapfont but width doesnt work

--- core/src/de/jjj/spacegame/MenuScreen.java
+++ core/src/de/jjj/spacegame/MenuScreen.java
@@ -3,19 +3,22 @@ package de.jjj.spacegame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MainScreen extends ScreenAdapter {
+public class MenuScreen extends ScreenAdapter {
 
     private SpriteBatch batch;
-    private Texture img;
     private Music bgMusic;
+    private BitmapFont title;
 
-    public MainScreen() {
+    public MenuScreen() {
        batch = new SpriteBatch();
-       img = new Texture("Ship/1_Triebwerke/Blaues_Schiff/0Rot1Blau.png");
+
+       title = new BitmapFont(Gdx.files.internal("BitmapFonts/TitleFont.fnt"));
 
        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Startmenu.wav"));
        bgMusic.setLooping(true);
@@ -27,14 +30,15 @@ public class MainScreen extends ScreenAdapter {
         ScreenUtils.clear(0, 0, 0, 1);
 
         batch.begin();
-        batch.draw(img, Gdx.graphics.getWidth()/2 - img.getWidth(), Gdx.graphics.getHeight()/2 - img.getHeight(), 150, 150);
+        // dont know how to get the width of the text
+        title.draw(batch, "D N A S I C", Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() - 200);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        title.dispose();
     }
 
 }
