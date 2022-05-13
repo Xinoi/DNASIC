52e1724b91b5486a53e68d65f7e81bce48d9c404
Jan Spennemann <janpeter.spennemann@gmail.com>
2022-05-13 15:19:38 +0200

    Added methods for saving and loading game data

---
+++ core/src/de/jjj/dnasic/GameData.java
@@ -0,0 +1,14 @@
+package de.jjj.dnasic;
+
+public class GameData {
+    private int highscore;
+    private String test;
+
+    public int getHighscore() {
+        return this.highscore;
+    }
+
+    public void setHighscore(int highscore){
+        this.highscore = highscore;
+    }
+}
