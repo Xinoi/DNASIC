a0ecf9f0536fb63653a90ccac54e03da4fc6c522
Zaunrith Johannes <JZaunrith@schule.lmgu.de>
2022-05-24 12:18:35 +0200

    changed music name

--- core/src/de/jjj/dnasic/MenuScreen.java
+++ core/src/de/jjj/dnasic/MenuScreen.java
@@ -92,7 +92,7 @@ public class MenuScreen extends ScreenAdapter {
         });
 
         // background Music
-        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Rebel – Alex-Productions.mp3"));
+        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Rebel.mp3"));
         bgMusic.setLooping(true);
         bgMusic.setVolume(0.3f);
         bgMusic.play();
