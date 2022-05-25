5786a2c1fdc7cec394cc59e480fd9c8f553211f1
Jan Spennemann <janpeter.spennemann@gmail.com>
2022-05-25 18:49:53 +0200

    Added speed control to PlayerShip

--- core/src/de/jjj/dnasic/PlayerShip.java
+++ core/src/de/jjj/dnasic/PlayerShip.java
@@ -9,10 +9,16 @@ public class PlayerShip extends Sprite {
     private float x;
     private float y;
 
+    private float speed;
+
     public PlayerShip(TextureRegion shipSkin, float x, float y) {
         super(shipSkin);
+
         this.x = x;
         this.y = y;
+
+        this.speed = 1f;
+
         super.setPosition(x, y);
     }
 
@@ -23,6 +29,13 @@ public class PlayerShip extends Sprite {
         return y;
     }
 
+    public float getSpeed(){
+        return this.speed;
+    }
+    public void increaseSpeed(float amount){
+        this.speed += amount;
+    }
+
     public void setCoords(float x, float y) {
         this.x = x;
         this.y = y;
@@ -30,13 +43,13 @@ public class PlayerShip extends Sprite {
 
     public void move(char direction, float amount) {
         if(direction == 'u') {
-            y += amount;
+            y += amount * this.speed;
         }else if(direction == 'd') {
-            y -= amount;
+            y -= amount * this.speed;
         }else if(direction == 'r') {
-            x += amount;
+            x += amount * this.speed;
         }else if(direction == 'l') {
-            x -= amount;
+            x -= amount * this.speed;
         }
 
         setCoords(x, y);
