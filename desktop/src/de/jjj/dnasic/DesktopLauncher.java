9c92420ec6772f7f48427936e7b936f2be5afbe7
itsSanoi <jonas1712b@gmail.com>
2022-05-26 14:53:00 +0200

    aded texure atlass for erde

--- desktop/src/de/jjj/dnasic/DesktopLauncher.java
+++ desktop/src/de/jjj/dnasic/DesktopLauncher.java
@@ -2,6 +2,8 @@ package de.jjj.dnasic;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
