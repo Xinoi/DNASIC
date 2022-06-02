a1b6caf0f03aa91798951c85e484db10e36b245e
Xinoi <zaunrithj@gmail.com>
2022-06-02 10:24:29 +0200

    added Level1 Class

---
+++ core/src/de/jjj/dnasic/screens/level/Level1.java
@@ -0,0 +1,28 @@
+package de.jjj.dnasic.screens.level;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Level1 extends ScreenAdapter {
+
+    public Level1() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(new Color(Color.BLACK));
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
