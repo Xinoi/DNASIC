8251fc74ba49df173ffc305fcaf5275890125d44
Xinoi <zaunrithj@gmail.com>
2022-06-02 10:41:32 +0200

    added Background

--- core/src/de/jjj/dnasic/screens/level/Level1.java
+++ core/src/de/jjj/dnasic/screens/level/Level1.java
@@ -1,18 +1,29 @@
 package de.jjj.dnasic.screens.level;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Level1 extends ScreenAdapter {
 
-    public Level1() {
+    private SpriteBatch batch;
+    private Sprite background;
 
+    public Level1() {
+        batch = new SpriteBatch();
+        background = new Sprite(new Texture(Gdx.files.internal("Images/Background/Background_1.jpg")));
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(new Color(Color.BLACK));
+        batch.begin();
+        background.draw(batch);
+        batch.end();
     }
 
     @Override
