22e3b3eb4215e108b981632890e15db4f24a3771
Xinoi <zaunrithj@gmail.com>
2022-06-02 11:25:45 +0200

    added Level Superclass

---
+++ core/src/de/jjj/dnasic/screens/level/Level.java
@@ -0,0 +1,85 @@
+package de.jjj.dnasic.screens.level;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import de.jjj.dnasic.ships.PlayerShip;
+
+public class Level extends ScreenAdapter implements InputProcessor {
+
+    float speedx = 0;
+    float speedy = 0;
+    private Sprite background;
+    private PlayerShip playerShip;
+    TextureAtlas playerAtlas;
+
+    public Level(Sprite BackgroundSprite) {
+        background = new Sprite(BackgroundSprite);
+        playerAtlas = new TextureAtlas(Gdx.files.internal("TextureAtlas/packed/Player_Ship/Player_Ship.atlas"));
+        playerShip = new PlayerShip(playerAtlas.findRegion("Ship_1"),300 , Gdx.graphics.getHeight() / 2 - playerAtlas.findRegion("Ship_1").getRegionHeight());
+        playerShip.scale(2); playerShip.rotate(-90);
+    }
+
+    public void renderAssets(SpriteBatch batch) {
+        batch.begin();
+        background.draw(batch);
+        playerShip.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void hide(){}
+
+    @Override
+    public void render(float delta) {
+        playerShip.move(speedx, speedy);
+    }
+
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.W) {
+            speedy = 1;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
--- core/src/de/jjj/dnasic/screens/level/Level1.java
+++ core/src/de/jjj/dnasic/screens/level/Level1.java
@@ -6,24 +6,24 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
+import de.jjj.dnasic.ships.PlayerShip;
 
-public class Level1 extends ScreenAdapter {
+public class Level1 extends Level{
 
     private SpriteBatch batch;
-    private Sprite background;
 
     public Level1() {
+        super(new Sprite(new Texture(Gdx.files.internal("Images/Background/Background_1.jpg"))));
         batch = new SpriteBatch();
-        background = new Sprite(new Texture(Gdx.files.internal("Images/Background/Background_1.jpg")));
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(new Color(Color.BLACK));
-        batch.begin();
-        background.draw(batch);
-        batch.end();
+        super.renderAssets(batch);
     }
 
     @Override
