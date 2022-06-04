0f3ff73cf9d70cfe55bbad9ad2b378f3754ceeb0
Jan Spennemann <janpeter.spennemann@gmail.com>
2022-06-04 17:33:49 +0200

    Added more NPC ships

--- core/src/de/jjj/dnasic/ships/Enemy1.java
+++ core/src/de/jjj/dnasic/ships/Enemy1.java
@@ -8,9 +8,8 @@ import de.jjj.dnasic.weapons.Weapon;
 public class Enemy1 extends EnemyShip {
 
     public Enemy1(float x, float y) {
-        super(new TextureAtlas(Gdx.files.internal("TextureAtlas/packed/NPC_Ship/NPC_Ship.atlas")).findRegion("Ship_1"), x, y, 1, 1, 50, new Weapon[2]);
+        super(new TextureAtlas(Gdx.files.internal("TextureAtlas/packed/NPC_Ship/NPC_Ship.atlas")).findRegion("Ship_1"), x, y, 1, 1, 50, new Weapon[1]);
 
         this.setWeapon(0, new Weapon(10, 2));
-        this.setWeapon(1, new Weapon(10, 2));
     }
 }
